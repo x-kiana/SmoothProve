22bded96d6353bfb8e4fb84a09bde2be74ff1199
Chester Gould <chestergould@protonmail.com>
2024-04-02 14:53:02 -0700

    fleshed out syntax

--- smooth_proove.pl
+++ smooth_proove.pl
@@ -1,16 +1,34 @@
+is_var(X) :- freeze(X, atom(X)).
+
 % is_atomic_prop(A) when A is syntactically an atomic proposition
-is_atomic_prop(X) :- freeze(X, atom(X)).
+is_atomic_prop(X) :- is_var(X).
 is_atomic_prop(A = B) :- is_set(A), is_set(B).
 is_atomic_prop(in(A, B)) :- is_set(A), is_set(B).
 
 % is_prop(A) when A is syntactically a proposition
 is_prop(A) :- is_atomic_prop(A).
+is_prop(top).
+is_prop(bot).
+is_prop(and(A, B)) :- is_prop(A), is_prop(B).
+is_prop(or(A, B)) :- is_prop(A), is_prop(B).
+is_prop(A -> B) :- is_prop(A), is_prop(B).
+is_prop(all(X, A)) :- is_var(X), is_prop(A).
+is_prop(exists(X, A)) :- is_var(X), is_prop(A).
 
 % is_set(S) when S is syntactically a set
 
+is_set(X) :- is_var(X).
+is_set(def(X, S)) :- is_var(X), is_prop(S).
+
 % is_env_elem(E) when E is syntactically an environment element
+is_env_elem(use(L, P)) :- is_var(L), is_prop(P).
+is_env_elem(set(S)) :- is_var(S).
+is_env_elem(prop(P)) :- is_var(P).
 
 % is_env(E) when E is syntactically an environment
+is_env(E) :- maplist(is_env_elem, E).
+
+% is_deriv(D) when D is syntactically a derivation tree
 
 % check_set(Env, Deriv, Set) holds when Deriv proves that Set is a set
 
